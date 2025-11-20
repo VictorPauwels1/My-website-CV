0f15779bc57d891b753bb9887cfa9505faf18b54
Pauwels Victor <23203@ecam.be>
2025-11-20 15:16:23 +0100

    first navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,7 @@
+"use client";
 import Link from "next/link";
 import { type ReactNode } from "react";
+import { useState } from "react";
 
 type NavLinksProps = {
     href: string
@@ -16,17 +18,38 @@ function NavLink(props: NavLinksProps) {
 }
 
 export default function Navbar(){
+    const [open, setOpen] = useState(false);
+
     return (
         <nav className="fixed top-0 left-0 w-full h-24 flex items-center justify-between px-6 z-50 
         bg-linear-to-r from-[#0fa319] to-[#ddff00] shadow-xl">
-            <img
+        
+        {/* Logo */}
+        <img
         className="h-20"
         src="https://patro.be/wp-content/uploads/sites/5/2018/11/patro-logo-300x195.png"
         alt="Logo"
         />
-        <ul className="flex gap-6">
+        
+        {/* boutton hamburger */}
+        <button 
+            className="text white font-bold text-4xl md:hidden"
+            onClick={() => setOpen(!open)}
+        >
+            ☰
+        </button>
+
+        <ul className={`
+          flex gap-6 transition-all duration-300 
+          md:flex md:static md:flex-row
+          absolute right-0 top-24 bg-[#0fa319] w-full md:w-auto 
+          flex-col items-center md:bg-transparent
+          ${open ? "flex" : "hidden md:flex"}
+        `}>
                 <NavLink href="/">Accueil</NavLink>
                 <NavLink href="/cv">CV</NavLink>
+                <NavLink href="/portfolio">Portfolio</NavLink>
+                <NavLink href="/contact">Contact</NavLink>
                 
         </ul>
         
